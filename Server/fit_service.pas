934e68ac5ba2f622cbc6777445c4b9110e02178c
Dmitry Morozov <dvmorozov@hotmail.com>
2020-05-02 17:26:05 +0200

    Duplicated methods and commented calls removed.
    #200

--- Server/fit_service.pas
+++ Server/fit_service.pas
@@ -2048,7 +2048,6 @@ var
     i:  longint;
     FT: TFitTask;
 begin
-    // metod vnutrenniy - ne vybrasyvaet isklyucheniya nedopustimogo sostoyaniya
     CreateTasks;
     InitTasks;
     for i := 0 to FTaskList.Count - 1 do
